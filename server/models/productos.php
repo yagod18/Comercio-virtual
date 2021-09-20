3349f0df622607b7d096489e03e824a3347b90ad
yagod <yagomlt@gmail.com>
2021-09-20 14:19:48 -0300

    cambios

--- server/models/productos.php
+++ server/models/productos.php
@@ -1,17 +1,12 @@
 <?php
-function mostrar_productos() {
-    return json_encode([
-        [
-            "nombre"=> "Coca cola", 
-            "precio" => 180, 
-            "cantidad"=>10
-        ],
-        [
-            "nombre"=> "La Gotita", 
-            "precio" => 150, 
-            "cantidad"=>5 
-        ]
-    ]    
-    );
+function mostrar_productos() 
+{
+     
+    $conection = new Mysqli("localhost", "root", "", "ecommerce");
 
+    $query = $conection->query("SELECT * FROM productos WHERE estatus = 1 /*solo se muestran los productos con estatus 1 (que se pueden vender) */");
+
+    //mysqli_close($conection);
+    $result = $query->fetch_all(1);
+    return json_encode($result);
 }
